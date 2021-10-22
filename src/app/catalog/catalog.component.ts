17b179514743eba33e836fb96f3ce64c99d1dcba
Seniv Pavlo <senivpavlo016@gmail.com>
2021-10-22 22:50:58 +0300

    Git init repo

---
+++ src/app/catalog/catalog.component.ts
@@ -0,0 +1,125 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../shared/item';
+
+const ITEMS: Item[] = [
+  {
+    id: '0',
+    name: 'Item A-1',
+    image: '../assets/images/A/A-1.png',
+    category: 'A',
+    featured: true,
+    label: 'L-1',
+    price: '4.99',
+    description: 'Lorem20',
+  },
+  {
+    id: '0',
+    name: 'Item A-1',
+    image: '../assets/images/A/A-1.png',
+    category: 'A',
+    featured: true,
+    label: 'L-1',
+    price: '4.99',
+    description: 'Lorem20',
+  },
+  {
+    id: '0',
+    name: 'Item A-1',
+    image: '../assets/images/A/A-1.png',
+    category: 'A',
+    featured: true,
+    label: 'L-1',
+    price: '4.99',
+    description: 'Lorem20',
+  },
+  {
+    id: '0',
+    name: 'Item A-1',
+    image: '../assets/images/A/A-1.png',
+    category: 'A',
+    featured: true,
+    label: 'L-1',
+    price: '4.99',
+    description: 'Lorem20',
+  },
+  {
+    id: '0',
+    name: 'Item A-1',
+    image: '../assets/images/A/A-1.png',
+    category: 'A',
+    featured: true,
+    label: 'L-1',
+    price: '4.99',
+    description: 'Lorem20',
+  },
+];
+@Component({
+  selector: 'app-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.scss'],
+})
+export class CatalogComponent implements OnInit {
+  
+  titleCatalog = 'Catalog';
+
+  items = ITEMS;
+
+  // selectedItem: Item = this.items[0];
+
+  // items: Item[] = [
+  //   {
+  //     id: '0',
+  //     name: 'Item A-1',
+  //     image: '../assets/images/A/A-1.png',
+  //     category: 'A',
+  //     featured: true,
+  //     label: 'L-1',
+  //     price: '4.99',
+  //     description: 'Lorem20',
+  //   },
+  //   {
+  //     id: '0',
+  //     name: 'Item A-1',
+  //     image: '../assets/images/A/A-1.png',
+  //     category: 'A',
+  //     featured: true,
+  //     label: 'L-1',
+  //     price: '4.99',
+  //     description: 'Lorem20',
+  //   },
+  //   {
+  //     id: '0',
+  //     name: 'Item A-1',
+  //     image: '../assets/images/A/A-1.png',
+  //     category: 'A',
+  //     featured: true,
+  //     label: 'L-1',
+  //     price: '4.99',
+  //     description: 'Lorem20',
+  //   },
+  //   {
+  //     id: '0',
+  //     name: 'Item A-1',
+  //     image: '../assets/images/A/A-1.png',
+  //     category: 'A',
+  //     featured: true,
+  //     label: 'L-1',
+  //     price: '4.99',
+  //     description: 'Lorem20',
+  //   },
+  //   {
+  //     id: '0',
+  //     name: 'Item A-1',
+  //     image: '../assets/images/A/A-1.png',
+  //     category: 'A',
+  //     featured: true,
+  //     label: 'L-1',
+  //     price: '4.99',
+  //     description: 'Lorem20',
+  //   },
+  // ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
